104c5f671ac7010cca7362bb84efa753db6219b9
Dou Yutao <yutaodou@gmail.com>
2022-01-15 10:35:53 +0800

    Support prettify/minify JSON

--- src/actions.ts
+++ src/actions.ts
@@ -4,6 +4,7 @@ import toDate from "date-fns/toDate";
 
 export type ActionResult = {
   value?: any;
+  json?: object;
   error?: Error;
 };
 
@@ -17,22 +18,40 @@ export const base64Decode = (input: string): ActionResult => {
   return { value: buffer.toString("utf-8") };
 };
 
-export const localTimestamp = () => {
+export const localTimestamp = (): ActionResult => {
   const now = new Date();
   const timestamp = getUnixTime(now);
   return { value: timestamp.toString() };
 };
 
-export const localDateISO8601 = () => {
+export const localDateISO8601 = (): ActionResult => {
   const now = new Date();
   return { value: formatISO(now) };
 };
 
-export const timestampToDateString = (input: string) => {
+export const timestampToDateString = (input: string): ActionResult => {
   try {
     const datetime = toDate(parseInt(input) * 1000);
     return { value: formatISO(datetime) };
   } catch {
     return { error: Error("Invalid timestamp value") };
+  }
+};
+
+export const prettifyJSON = (input: string): ActionResult => {
+  try {
+    const parsed = JSON.parse(input);
+    return { json: parsed };
+  } catch {
+    return { error: Error("Invalid JSON input") };
+  }
+};
+
+export const minifyJSON = (input: string): ActionResult => {
+  try {
+    const parsed = JSON.parse(input);
+    return { value: JSON.stringify(parsed) };
+  } catch {
+    return { error: Error("Invalid JSON input") };
   }
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,14 @@
-import { List } from "@raycast/api"
-import { base64Decode, base64Encode, localDateISO8601, localTimestamp, timestampToDateString } from "./actions"
-import ClipboardAction from "./ClipboardAction"
+import { List } from "@raycast/api";
+import {
+  base64Decode,
+  base64Encode,
+  localDateISO8601,
+  localTimestamp,
+  minifyJSON,
+  prettifyJSON,
+  timestampToDateString,
+} from "./actions";
+import ClipboardAction from "./ClipboardAction";
 
 export default function Command() {
   return (
@@ -11,14 +19,18 @@ export default function Command() {
           icon="list-icon.png"
           title="Base64 Encode"
           subtitle="Base64 encode text from clipboard"
-          actions={<ClipboardAction title="Base64 Encoded" action={base64Encode} />}
+          actions={
+            <ClipboardAction title="Base64 Encoded" action={base64Encode} />
+          }
         />
         <List.Item
           key="2"
           icon="list-icon.png"
           title="Base64 Decode"
           subtitle="Base64 decode text from clipboard"
-          actions={<ClipboardAction title="Base64 Decoded" action={base64Decode} />}
+          actions={
+            <ClipboardAction title="Base64 Decoded" action={base64Decode} />
+          }
         />
       </List.Section>
 
@@ -28,21 +40,48 @@ export default function Command() {
           icon="list-icon.png"
           title="Timestamp"
           subtitle="Return the time in seconds since the epoch as a floating point number. "
-          actions={<ClipboardAction title="Timestamp" action={localTimestamp} />}
+          actions={
+            <ClipboardAction title="Timestamp" action={localTimestamp} />
+          }
         />
         <List.Item
           key="4"
           icon="list-icon.png"
           title="Local Date"
           subtitle="Return local date in ISO8601 format"
-          actions={<ClipboardAction title="Local Date" action={localDateISO8601} />}
+          actions={
+            <ClipboardAction title="Local Date" action={localDateISO8601} />
+          }
         />
         <List.Item
           key="5"
           icon="list-icon.png"
           title="Timestamp to Date"
           subtitle="Convert timestam to date and return in ISO8601 format"
-          actions={<ClipboardAction title="Timestamp to Date" action={timestampToDateString} />}
+          actions={
+            <ClipboardAction
+              title="Timestamp to Date"
+              action={timestampToDateString}
+            />
+          }
+        />
+      </List.Section>
+      <List.Section title="Format">
+        <List.Item
+          key="5"
+          icon="list-icon.png"
+          title="Prettify JSON"
+          subtitle="Pretty JSON text from clipboard"
+          actions={
+            <ClipboardAction title="Pretty JSON" action={prettifyJSON} />
+          }
+        />
+        <List.Item
+          key="5"
+          icon="list-icon.png"
+          title="Minify JSON"
+          subtitle="Minify JSON text from clipboard"
+          actions={<ClipboardAction title="Minify JSON" action={minifyJSON} />}
         />
       </List.Section>
     </List>
